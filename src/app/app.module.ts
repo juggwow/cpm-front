6140a5f8bc8b265f53cb875f203f59e73c79d04f
“naczaaja” <chirayu.chomsri@gmail.com>
2022-12-21 16:28:52 +0700

    add theme-lura

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TopbarComponent } from './components/topbar/topbar.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { PageRadComponent } from './components/page-rad/page-rad.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TopbarComponent,
-    SidebarComponent
+    SidebarComponent,
+    PageRadComponent
   ],
   imports: [
     BrowserModule,
