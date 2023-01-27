0ffc2520dc6b0360d1601496af3250a599857370
Yodsapon Naree <yodsapon.naree@icloud.com>
2023-01-27 09:56:02 +0700

    post mock data

---
+++ src/app/models/form.model.ts
@@ -0,0 +1,17 @@
+
+
+export interface Form {
+    itemID: number
+    arrival: string
+    inspection: string
+    taskMaster: string
+    invoice: string
+    quantity: number
+    country: string
+    manufacturer: string
+    model: string
+    serial: string
+    peano: string
+    createby: string
+    status: number
+}
--- src/app/pages/form/form.component.ts
+++ src/app/pages/form/form.component.ts
@@ -1,5 +1,5 @@
 
-import { Component,OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TableModule } from 'primeng/table';
 import { ButtonModule } from 'primeng/button';
@@ -10,27 +10,64 @@ import { RouterModule, Router } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { PaginatorModule } from 'primeng/paginator';
 import {
-    debounceTime,
-    distinctUntilChanged,
-    map,
-    scan,
-    shareReplay,
-    switchMap,
-    tap,
+  debounceTime,
+  distinctUntilChanged,
+  map,
+  scan,
+  shareReplay,
+  switchMap,
+  tap,
+  take,
 } from 'rxjs/operators';
 import { AutoCompleteModule } from 'primeng/autocomplete';
 import { BehaviorSubject, Subject } from 'rxjs';
+import { FormService } from './../../services/rad-form'
 @Component({
+  providers: [FormService],
   selector: 'app-form',
-  standalone : true,
+  standalone: true,
   templateUrl: './form.component.html',
   styleUrls: ['./form.component.scss']
 })
 export class FormComponent implements OnInit {
 
-  constructor() { }
+  constructor(private FormService: FormService) {
+
+  }
 
   ngOnInit(): void {
   }
 
+
+
+  SendToPEA() {
+    this.FormService.addNewForm(
+      {
+        "itemID": 10011,
+        "arrival": "2021-09-09",
+        "inspection": "2021-09-09",
+        "taskMaster": "ผู้ควบคุมงาน บริษัท",
+        "invoice": "A123",
+        "quantity": 10,
+        "country": "span",
+        "manufacturer": "sintec",
+        "model": "y2",
+        "serial": "ad1233",
+        "peano": "pea-123",
+        "createby": "คนสร้าง เอกสาร",
+        "status": 1
+      }
+
+    ).pipe(
+      take(1),
+      tap(() => {
+        console.log('..')
+        // this.appToastService.successToast();
+        // this.router.navigate(['/']);
+      })
+    )
+      .subscribe({
+        error: () => console.log('error')//this.appToastService.errorToast(),
+      });
+  }
 }
