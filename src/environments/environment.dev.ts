d139ab826247e9931f59878bd43217d1fac9dc79
aunzyme <aunpeac3@gmail.com>
2023-08-13 04:01:23 +0700

    EDIT Link

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
-    apiUrl : 'https://cpm-rad-api-ing-dev.pea.co.th/api',
+    apiUrl : 'https://cpm-rad-api-ing-dev.pea.co.th/api/v1',
     authApiUrl: 'https://cpm-rad-api-ing-dev.pea.co.th/auth',
   };
