16a0658aa1be532bac7fff1d05aa3585c8b1c0f1
Pittaya Chon <pittayachon@gmail.com>
2023-08-22 08:35:05 +0700

    fix style

---
+++ src/environments/environment.local.ts
@@ -0,0 +1,18 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: true,
+  apiUrl : 'https://cpm-rad-api-ing-dev.pea.co.th/api/v1',
+  authApiUrl: 'https://cpm-rad-api-ing-dev.pea.co.th/auth',
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
